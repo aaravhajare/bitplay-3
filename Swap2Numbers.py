38f733ce194db7a19c55479e9da1fa7178823b7e
aaravhajare <hajareaarav66@gmail.com>
2025-08-24 09:31:34 +0530

    upadete

---
+++ Swap2Numbers.py
@@ -0,0 +1,17 @@
+
+def swap(a,b) :
+
+    a = a ^ b 
+    b = a ^ b
+    a = a ^ b
+    print("a =" , a , "b = " , b)
+
+def swap2 (a,b) :
+
+    a = (a & b) + (a | b)
+    b = a + ~b + 1
+    a = a + ~b + 1
+    print("a =" , a , "b = " , b)
+
+swap(1,2)
+swap2(1,2)
